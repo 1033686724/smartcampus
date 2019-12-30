a206a85a70264be7b5728eda421be986c14e9ca3
zhangsz <932087228@qq.com>
2019-12-30 09:41:26 +0800

    第一次

--- src/main/java/com/briup/smartcampus/mapper/ex/QuestionnaireEXMapper.java
+++ src/main/java/com/briup/smartcampus/mapper/ex/QuestionnaireEXMapper.java
@@ -5,6 +5,7 @@ import com.briup.smartcampus.bean.Questionnaire;
 import java.util.List;
 
 public interface QuestionnaireEXMapper {
+    List<Questionnaire> selectAll() throws RuntimeException;//查询所有
     List<Questionnaire> selectNameById(int id) throws RuntimeException;//根据id查询
     List<Questionnaire> selectNameByWord(String word)throws RuntimeException;//根据关键字查询
 }
