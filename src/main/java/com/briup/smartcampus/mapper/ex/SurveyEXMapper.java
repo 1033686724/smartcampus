3bb9b8b4cf59e621351e4492b0c570cd675a7fdc
lifp <1016785274@qq.com>
2019-12-31 16:14:19 +0800

    课调管理

--- src/main/java/com/briup/smartcampus/mapper/ex/SurveyEXMapper.java
+++ src/main/java/com/briup/smartcampus/mapper/ex/SurveyEXMapper.java
@@ -1,5 +1,14 @@
 package com.briup.smartcampus.mapper.ex;
 
+import com.briup.smartcampus.bean.*;
+import com.briup.smartcampus.bean.Class;
+import com.briup.smartcampus.bean.ex.SurveyEX;
+
+import java.util.List;
+
 public interface SurveyEXMapper {
+    List<SurveyEX> findAll() throws RuntimeException;
+
+
 
 }
--- src/main/java/com/briup/smartcampus/service/impl/SurveyServiceImpl.java
+++ src/main/java/com/briup/smartcampus/service/impl/SurveyServiceImpl.java
@@ -1,9 +1,53 @@
 package com.briup.smartcampus.service.impl;
 
+import com.briup.smartcampus.bean.Course;
+import com.briup.smartcampus.bean.Survey;
+import com.briup.smartcampus.bean.ex.SurveyEX;
+import com.briup.smartcampus.mapper.SurveyMapper;
+import com.briup.smartcampus.mapper.ex.SurveyEXMapper;
 import com.briup.smartcampus.service.ISurveyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SurveyServiceImpl implements ISurveyService {
+    @Autowired
+    private SurveyEXMapper surveyEXMapper;
+    @Autowired
+    private SurveyMapper surveyMapper;
 
+    @Override
+    public List<SurveyEX> selectAll() throws RuntimeException {
+        List<SurveyEX> list =
+        surveyEXMapper.findAll();
+
+        return list;
+    }
+
+    @Override
+    public void SaveOrUpdate(Survey survey) throws RuntimeException {
+
+        if (survey == null){
+            throw new  RuntimeException("参数不能为空");
+        }else if (survey.getId() == null){
+            surveyMapper.insert(survey);
+
+        }else {
+            surveyMapper.updateByPrimaryKey(survey);
+        }
+    }
+
+    @Override
+    public void delete(int id) throws RuntimeException {
+        surveyMapper.deleteByPrimaryKey(id);
+    }
+
+    @Override
+    public void deleteBatch(int[] ids) throws RuntimeException {
+        for(int id : ids) {
+            surveyMapper.deleteByPrimaryKey(id);
+        }
+    }
 }
